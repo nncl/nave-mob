0bb139acccccdcc3f00b5376e0d98ca3f08b7db7
logonrm <logonrm@fiap.com.br>
2017-06-21 22:13:53 -0300

    Initial commit

---
+++ Assets/Scripts/Fundo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fundo : MonoBehaviour {
+
+	public float limiteY;
+	public float posicaoY;
+	public float velocidade;
+
+	void Update () {
+
+		// Movimenta o fundo para a esquerda
+		transform.Translate (Vector2.down * velocidade * Time.deltaTime); 
+
+		// Reposiciona o fundo para ficar em looping
+		if(transform.position.y <= limiteY){
+			transform.position = new Vector3 (0.0f, posicaoY, 0.0f);
+		}
+
+	}
+}
---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour {
+
+	public float velocidade;
+	public float alturaY;
+	Vector3 posicaoMouse;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		ControleTouch ();
+	}
+
+	void ControleTouch () {
+		// Pixel p/ cartesiano
+		// Input fire 1 -> ctrl lado esquerdo, mouse botao esq ou touch
+		if (Input.GetButton("Fire1")) {
+			posicaoMouse = Input.mousePosition;
+			posicaoMouse = Camera.main.ScreenToWorldPoint (posicaoMouse);
+			posicaoMouse = new Vector3 (posicaoMouse.x, posicaoMouse.y + alturaY, posicaoMouse.z);
+
+			// Transporta o jogador para onde a tela é tocada
+			transform.position = Vector2.Lerp (transform.position, posicaoMouse, velocidade * Time.deltaTime);
+		}
+	}
+}
