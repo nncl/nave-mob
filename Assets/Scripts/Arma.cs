15dde48f6c52d593e9cce57976978935c274eda2
logonrm <logonrm@fiap.com.br>
2017-06-21 22:57:43 -0300

    Adding bullet and weapons

---
+++ Assets/Scripts/Arma.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arma : MonoBehaviour {
+
+	public float intervalo;
+	public GameObject projetilPrefab;
+
+	IEnumerator Start () {
+		yield return new WaitForSeconds (intervalo);
+		Instantiate (projetilPrefab, transform.position, transform.rotation);
+
+		StartCoroutine (Start());
+	}
+}
---
+++ Assets/Scripts/Projetil.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projetil : MonoBehaviour {
+
+	public float velocidade;
+	public float intervalo;
+
+	// Use this for initialization
+	void Start () {
+		// Aciona o tempo p destruir o projetil
+		Destroy(gameObject, intervalo);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector2.up * velocidade * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D c) {
+		// Destroi o projetil somente se ele colidir com meteoro
+		if (c.gameObject.tag == "Meteoro") {
+			Destroy (gameObject);
+		}
+	}
+}
